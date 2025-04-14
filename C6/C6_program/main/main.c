8591f931cbf627486ddb01f5721ca21828cf10c8
richekalen <richard.kalen.gura@gmail.com>
2025-04-14 10:03:09 +0200

    Fungerar bättre med sekvenschecken

--- C6/C6_program/main/main.c
+++ C6/C6_program/main/main.c
@@ -5,34 +5,33 @@
 btn_i_handle btn1;
 btn_i_handle btn2;
 btn_i_handle btn3;
-int correctsequence[]= {
-    1,2,3,3,2,1
-};
+int correctsequence[] = {
+    1, 2, 3, 3, 2, 1};
 
 void app_main(void)
 {
-    btn1 = btn_i_init(4, PULLUP, 50);
-    btn2 = btn_i_init(21, PULLDOWN, 50);
-    btn3 = btn_i_init(13, PULLDOWN, 50);
+    btn1 = btn_i_init(4, PULLUP, 100);
+    btn2 = btn_i_init(21, PULLUP, 100);
+    btn3 = btn_i_init(13, PULLUP, 100);
     int attempt = 0;
-    while (attempt < 3){
+    while (attempt < 3)
+    {
 
         int currentIndex = 0;
         int user_sequence[6];
         int lastindex = 0;
-        
+
         while (currentIndex < 6)
         {
             btn_i_update(btn1);
             btn_i_update(btn2);
             btn_i_update(btn3);
-            //lägg in vtaskDelay
+            // lägg in vtaskDelay
             vTaskDelay(pdMS_TO_TICKS(10));
 
-            
             if (btn_pressed(btn1))
             {
-                printf("btn 1\n");
+                // printf("btn 1\n");
                 user_sequence[currentIndex] = 1;
                 currentIndex++;
                 btn_clear_flag(btn1);
@@ -49,21 +48,21 @@ void app_main(void)
                 currentIndex++;
                 btn_clear_flag(btn3);
             }
-        }
-        if (currentIndex != lastindex)
-        {
-            lastindex ++;
-            
-                if (user_sequence[currentIndex] == correctsequence[currentIndex])
+            if (currentIndex != lastindex)
+            {
+                lastindex++;
+
+                if (user_sequence[currentIndex - 1] == correctsequence[currentIndex - 1])
                 {
                     printf("Correct\n");
                 }
                 else
                 {
                     printf("Wrong!\n");
+                    currentIndex = 0;
+                    lastindex = 0;
                 }
-            
+            }
         }
     }
-        
 }
